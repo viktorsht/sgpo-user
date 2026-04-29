1cf4f7b39cb5c560b9bb1d596ddc9a1dd8f13334
viktorsht <viktorshantus@gmail.com>
2026-04-29 13:34:01 -0300

    add get

--- src/main/java/com/sgpo/ms/user/config/SecurityConfig.java
+++ src/main/java/com/sgpo/ms/user/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/users/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/login").permitAll()
-                        .requestMatchers(HttpMethod.GetMapping, "/health").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/health").permitAll()
                         .anyRequest().authenticated())
                 .csrf(csrf -> csrf.disable())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
