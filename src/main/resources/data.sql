27457944a56a76791246aace2a2e353f8b44c3f4
viktorsht <viktorshantus@gmail.com>
2026-05-02 16:58:07 -0300

    init banco

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,8 +5,3 @@ CREATE TABLE IF NOT EXISTS roles (
 
 INSERT IGNORE INTO roles (role_id, name) VALUES (1, 'Admin');
 INSERT IGNORE INTO roles (role_id, name) VALUES (2, 'Client');
-
-INSERT IGNORE INTO users (email, name, phone, password) VALUES
-('cliente1@gmail.com', 'Cliente 1', '11999999991', '123456'),
-('cliente2@gmail.com', 'Cliente 2', '11999999992', '123456'),
-('cliente3@gmail.com', 'Cliente 3', '11999999993', '123456');
